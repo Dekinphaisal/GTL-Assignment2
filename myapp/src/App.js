3fe69258ceb7e9aba999e670dbf36a23d2abbd43
Dekinphaisal <phaisalsalif@gmail.com>
2020-08-13 11:11:43 -0700

    sec commit

---
+++ myapp/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './component/css/components.css';
+import './component/css/layout.css';
+import './component/css/global.css';
+import './component/css/sample-wallpaper.css';
+
+import Mycode from './component/mycode';
+
+function App() {
+  return (
+    <div>
+
+        <Mycode/>
+        
+    </div>
+  );
+}
+
+export default App;
